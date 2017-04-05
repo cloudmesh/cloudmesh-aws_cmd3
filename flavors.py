e2748005dea7ffeb84e09a850946b6a627c7c78c
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2017-04-05 11:59:29 -0400

    new:dev: add beautifulsoup as requirement for aws

---
+++ flavors.py
@@ -0,0 +1,8 @@
+"""The AWS API does not provide a way to retrieve all available instance types.
+
+This module provides such an implementation by scraping the **Instance
+Types Matrix** athttps://aws.amazon.com/ec2/instance-types
+"""
+
+
+import 
